a05ea3586db97890ea64f3f4a5865550f10b1505
Mehul Kapoor <mehulkapoor.26@gmail.com>
2026-07-22 18:44:05 +0530

    Time: 2 ms (2.42%), Space: 43.5 MB (36.24%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int scoreOfString(String s) {
+       int score=0;
+        for(int i=0;i<s.length()-1;i++){
+            int diff = s.charAt(i) - s.charAt(i+1);
+            score+=Math.abs(diff);
+        }
+        return score; 
+    }
+}
